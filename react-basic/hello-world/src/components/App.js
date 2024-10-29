c9eb09dfa1b531ec721de826d4deeb2a1b8cc63b
code826 <code.for.fun.1997@gmail.com>
2024-10-29 22:41:39 +0530

    basic-app-ready-create-react-app

---
+++ react-basic/hello-world/src/components/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const App = () =>{
+  return (
+    <>
+        <Navbar/>
+        <h1>Hello in react</h1>
+    </>
+
+  )
+}
+
+export default App;
---
+++ react-basic/hello-world/src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () =>{
+    return (
+        <ol className="nav">
+            <li>home</li>
+            <li>contact</li>
+        </ol>
+    )
+
+}
+export default Navbar;
